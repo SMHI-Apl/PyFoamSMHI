f61690ba9bbac5f63e0b2ef82cefeb9b647d350a
David Segersson <david.segersson@smhi.se>
2015-05-19 14:24:51 +0200

    added init-files for tools

---
+++ PyFoamSMHI/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: latin-1 -*-
+"""A package to extend PyFoam by G Scheider."""
+
+import pkg_resources
+__version__ = pkg_resources.get_distribution('PyFoamSMHI').version
---
+++ PyFoamSMHI/tools/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Utility commands for OpenFOAM related stuff."""
